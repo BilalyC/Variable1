e98368bfa418260638582d8abaa4748a6818d586
BilalyC <bilaly.camara@outlook.com>
2018-12-17 14:31:11 +0100

    Exercice 1 Complet

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = 2;
+            b = 5;
+
+            Console.WriteLine("La valeur de a est " + a);
+            Console.WriteLine("La valeur de b est " + b);
+
+            Console.WriteLine("La somme des deux valeurs est " + (a + b));
+        }
+    }
+}
